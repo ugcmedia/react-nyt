c3165bc4f4f249c3479ad81a25a0f59003f8f091
Stephan Azevedo <webdevroot@stephans-mbp.attlocal.net>
2017-12-02 00:21:44 -0500

    react homework

---
+++ client/src/components/Main.js
@@ -0,0 +1,17 @@
+import React, {Component}  from 'react';
+
+
+
+class Main extends Component {
+    render() {
+        return(
+            <stephane className="male" id="anything">
+                <div className="row">
+                    <p className="paragraph">anything anthing</p>
+                </div>
+            </stephane>
+        );
+    }
+}
+
+export default Main;
